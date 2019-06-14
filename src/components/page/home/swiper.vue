9900179f17b114eebc2d6175fcf4bfaa0d4c2f94
nifan <nifan1950333426@qq.com>
2019-06-14 21:05:28 +0800

    v-for循环数据获得轮播图

--- src/components/page/home/swiper.vue
+++ src/components/page/home/swiper.vue
@@ -1,10 +1,7 @@
 <template>
 <div class="wraper">
     <swiper :options="swiperOption">
-    <swiper-slide><img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/667757f979fa38b12c4986f9930b6695.jpg_750x200_6cca7add.jpg"></swiper-slide>
-    <swiper-slide><img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg"></swiper-slide>
-    <swiper-slide><img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/292b7afd8042e0fde68b5c09d4a22d27.jpg_750x200_42d7391a.jpg"></swiper-slide>
-    <swiper-slide><img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/fec63fd018410720a081d0a651831e55.jpg_750x200_8fc4c0d3.jpg"></swiper-slide>
+    <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.url"></swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
   </swiper>
 </div>
@@ -18,9 +15,19 @@ export default {
       swiperOption: {
           pagination: '.swiper-pagination',
           loop : true
-      }
+    },
+    swiperList: [
+                {id: '001', 
+                url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/667757f979fa38b12c4986f9930b6695.jpg_750x200_6cca7add.jpg'},
+                {id: '002', 
+                url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg'},
+                {id: '003', 
+                url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/292b7afd8042e0fde68b5c09d4a22d27.jpg_750x200_42d7391a.jpg'},
+                {id: '004', 
+                url: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/fec63fd018410720a081d0a651831e55.jpg_750x200_8fc4c0d3.jpg'}
+            ]
+        }
     }
-  }
 }
 </script>
 
